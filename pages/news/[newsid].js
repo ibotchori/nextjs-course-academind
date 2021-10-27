7be8d960808ae01fc847128d1d0dbc087c54d15f
Irakli <koregm@gmail.com>
2021-10-27 16:19:28 +0400

    Extracting Dynamic Route Data

--- pages/news/[newsid].js
+++ pages/news/[newsid].js
@@ -1,9 +1,15 @@
 // our-domain.com/news/something-important
 
+import { useRouter } from "next/router";
 
 const DetailPage = () => {
-    return <h1>The Detail Page</h1>;
-  };
-  
-  export default DetailPage;
-  
+  const router = useRouter();
+
+  const newsId = router.query.newsId; // <-- get value from URL
+
+  // send a request to the backed API to fetch the news item with newsId
+
+  return <h1>The Detail Page</h1>;
+};
+
+export default DetailPage;
